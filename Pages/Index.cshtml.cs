e639418b88720b1f9c2b57605ca0e83ed90cf25d
Ye Na Hwang <yhwang20@my.bcit.ca>
2023-09-20 11:37:47 +0900

    Initial commit on main branch

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using FindZodiac.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FindZodiac.Pages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public int? Year { get; set;}
+    public string? Zodiac { get; set;}
+
+    public bool IsValidYear  => Year.HasValue && Year >= 1900 && Year <= (DateTime.Now.Year + 1);
+
+   
+   public void OnPost()
+   {
+        if(!IsValidYear)
+        {
+            ModelState.AddModelError("Year", "Enter a valid year between 1900 and " + (DateTime.Now.Year + 1));
+        }
+
+        Zodiac = Utils.GetZodiac(Year!.Value);
+   }
+    public void OnGet()
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+"# FindZodiac" 
+"# FindZodiac" 
+"# FindZodiac" 
